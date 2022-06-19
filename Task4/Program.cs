5bffd1cd9e28943da78445ca196a382b2f44698a
Shumilova Tatyana <star28@inbox.ru>
2022-06-19 12:40:18 +0300

    Дополнена и решена задача 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,21 +4,25 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+
 Console.WriteLine("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
-if (num2 < num1 > num3);
+if (num2 < num1) 
+if (num3 < num1)
 {
 Console.WriteLine("{0} максимальное число", num1);
 }
-if (num1 < num2 > num3)
+if (num1 < num2)
+if (num3 < num2)
 {
 Console.WriteLine("{0} максимальное число", num2);
 }
-if (num1 < num3 > num2)
+if (num1 < num3)
+if (num2 < num3)
 {
 Console.WriteLine("{0} максимальное число ", num3);
 }
